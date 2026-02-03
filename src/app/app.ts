d042c97251fb7df979ada4dc680ef84e2dde4310
cteheranr <kmilo.teheran@gmail.com>
2026-02-03 14:37:13 -0500

    cambios en la hora

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
